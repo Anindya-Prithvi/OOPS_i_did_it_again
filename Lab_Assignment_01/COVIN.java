9cefec18805b2fccca6c71191822f03316c19ecc
Anindya <anindya20024@iiitd.ac.in>
2021-10-08 02:50:30 +0530

    init

---
+++ Lab_Assignment_01/COVIN.java
@@ -0,0 +1,16 @@
+public class COVIN{
+    public static void main(String[] args){
+        //initialize portal
+        System.out.println("COVIN Portal Initialized...\n"
+        .concat("1. Add Vaccine\n")
+        .concat("2. Register Hospital\n")
+        .concat("3. Register Citizen\n")
+        .concat("4. Add slot for vaccination\n")
+        .concat("5. Book slot for vaccination\n")
+        .concat("6. List all slots for a hospital\n")
+        .concat("7. Check Vaccination Status\n")
+        .concat("8. Exit\n")
+        .concat("--------------------------------\n")
+        );
+    }
+}
