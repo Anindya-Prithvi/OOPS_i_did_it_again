7bbc6dec995f04d609bdc618ba301b0004730cf4
Anindya <anindya20024@iiitd.ac.in>
2021-10-08 13:10:49 +0530

    more params,meths

--- Lab_Assignment_01/assets/Hospital.java
+++ Lab_Assignment_01/assets/Hospital.java
@@ -1,23 +1,42 @@
 package Lab_Assignment_01.assets;
 
 public class Hospital {
-    private final String pincode; //pincode 002013
-    private final String name;
+    private static int id_int;
 
-    public Hospital(String pincode, String name){
+    private final char[] pincode; //pincode 002013
+    private final String name;
+    private final char[] huid; //000000
+
+    public Hospital(char[] pincode, String name, char[] huid){
+        ++id_int;
         this.pincode = pincode;
         this.name = name;
+        this.huid = update_huid();
+    }
+
+    private char[] update_huid(){
+        String temp = String.valueOf(id_int); //temp should have atmost 6 digits
+        int temp_length = temp.length();
+        for(int i=0; i<6-temp_length; i++){
+            temp = 0+temp;
+        }
+        char[] huid = temp.toCharArray();
+        return huid;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getPincode() {
+    public char[] getPincode() {
         return pincode;
     }
 
     public boolean upload_slot(){
         return true;
+    }
+
+    public char[] getHuid() {
+        return huid;
     }
 }
--- Lab_Assignment_01/assets/slot.java
+++ Lab_Assignment_01/assets/slot.java
@@ -1,14 +1,16 @@
 package Lab_Assignment_01.assets;
 
 public class Slot {
+    private final char[] hospital_id;
     private final int day;
     private final Vaccine vaccine;
     private int available_quantity;
 
-    public Slot(int day, Vaccine vaccine, int available_quantity){
+    public Slot(int day, Vaccine vaccine, int available_quantity, char[] hospital_id){
         this.day = day;
         this.vaccine = vaccine;
         this.available_quantity = available_quantity;
+        this.hospital_id = hospital_id;
     }
 
     public int getAvailable_quantity() {
@@ -26,5 +28,9 @@ public class Slot {
     public void used_vaccine() {
         --available_quantity;
         return;
+    }
+
+    public char[] getHospital_id() {
+        return hospital_id;
     }
 }
