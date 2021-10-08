d2f7ed64614457a69e532c1928a78a0a27d7f85e
Anindya <anindya20024@iiitd.ac.in>
2021-10-08 14:36:17 +0530

    menuscreen, git ignore problem

--- Lab_Assignment_01/COVIN.java
+++ Lab_Assignment_01/COVIN.java
@@ -1,8 +1,10 @@
 package Lab_Assignment_01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 public class COVIN{
-    public static void main(String[] args){
-        //initialize portal
-        System.out.println("COVIN Portal Initialized...\n"
+    private static final String endline = "--------------------------------\n";
+    private static final String menu = ""
         .concat("1. Add Vaccine\n")
         .concat("2. Register Hospital\n")
         .concat("3. Register Citizen\n")
@@ -11,8 +13,15 @@ public class COVIN{
         .concat("6. List all slots for a hospital\n")
         .concat("7. Check Vaccination Status\n")
         .concat("8. Exit\n")
-        .concat("--------------------------------\n")
-        );
+        .concat(endline);
+    public static void main(String[] args){
+        //initialize portal
+        Scanner sc = new Scanner(System.in);
+        System.out.print("COVIN Portal Initialized...\n"
+        .concat(endline));
+        System.out.print(menu);
+        
+
     }
 
     //here we shall make Hospitals and citizens (staker)
